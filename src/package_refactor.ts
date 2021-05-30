d57d7074dc8d5769654cf0c5b472dfa10df535cd
Brian Takita <brian.takita@gmail.com>
2021-05-30 13:39:32 -0400

    ## 7.0.0
    ### Major Changes
    
    - \$ suffix for stores
    - \_ suffix for factory functions
    
    ### Patch Changes
    
    - Updated dependencies [undefined]
    - Updated dependencies [undefined]
    - @ctx-core/array@21.0.0

--- src/package_refactor.ts
+++ src/package_refactor.ts
@@ -1,7 +1,7 @@
 import fs from 'fs'
 import globby from 'globby'
 export async function package_refactor() {
-	const promise_a1 = (await globby('packages/*/package.json')).map(
+	const promise_a = (await globby('packages/*/package.json')).map(
 		async package_path=>{
 			const package_json = (await fs.promises.readFile(package_path)).toString()
 			let pkg = JSON.parse(package_json)
@@ -68,5 +68,5 @@ export async function package_refactor() {
 				await fs.promises.writeFile(package_path, JSON.stringify(pkg, null, '\t'))
 			}
 		})
-	await Promise.all(promise_a1)
+	await Promise.all(promise_a)
 }
