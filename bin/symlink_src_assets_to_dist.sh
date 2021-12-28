382bf736c6ff251cca8133835ce4e24c17fe369e
Brian Takita <brian.takita@gmail.com>
2021-12-28 17:12:59 -0600

    lib instead of dist

--- bin/symlink_src_assets_to_dist.sh
+++ bin/symlink_src_assets_to_dist.sh
@@ -2,11 +2,11 @@
 Dir=$1
 PATHS=$(cd $Dir/src && find * | grep -e \.svelte -e \.scss)
 echo $Dir
-mkdir -p $Dir/dist
+mkdir -p $Dir/lib
 rm -f $Dir/src/**/*.svelte
 rm -f $Dir/src/**/*.scss
 for path in $PATHS; do
-	Rel=$(python -c 'import os, sys; print(os.path.relpath(*sys.argv[1:]))' $Dir/src/$path $(dirname $Dir/dist/$path))
-	(cd $Dir/dist/$(dirname $path) && ln -sf $Rel )
-	(cd $Dir && git add -f dist/$path)
+	Rel=$(python -c 'import os, sys; print(os.path.relpath(*sys.argv[1:]))' $Dir/src/$path $(dirname $Dir/lib/$path))
+	(cd $Dir/lib/$(dirname $path) && ln -sf $Rel )
+	(cd $Dir && git add -f lib/$path)
 done
